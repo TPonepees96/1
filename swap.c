58e4f0578dbca7ccde1d5c73b7485cf29df651b0
TPonepees96 <114491890+TPonepees96@users.noreply.github.com>
2022-09-28 23:25:24 +0530

    Add files via upload
    Swapping two numbers using a temporary variable

---
+++ swap.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int x,y,temp;
+    printf("Enter first number:");
+    scanf("%d",&x);
+    printf("Enter second number:");
+    scanf("%d",&y);
+
+    temp=x;
+    x=y;
+    y=temp;
+
+    printf("\nAfter swapping first number:%d",x);
+    printf("\nSecond number:%d",y);
+
+}
