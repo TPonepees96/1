12fb061e7de87bebd700472c0ff13c6f94eb316c
TPonepees96 <114491890+TPonepees96@users.noreply.github.com>
2022-09-29 23:52:04 +0530

    Create calculator.c
    very basic very very very very basic calculator

---
+++ calculator.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+    int x;
+    float a,b,F,G,H,I;
+   
+    printf("Enter first number:");
+    scanf("%f",&a);
+    printf("Enter second number:");
+    scanf("%f",&b);
+    printf("What operation do you want to perform?:");
+    scanf("%d",&x);
+    F=a+b;
+    G=a-b;
+    H=a*b;
+    I=a/b;
+    if(x==1){
+        printf("result:%f",F);
+    }
+    else if (x==2)
+    {
+        printf("result:%f",G);
+    } else if(x==3){
+        printf("result:%f",H);
+    } else if(x==4){
+        printf("result:%f",I);
+    } else {
+        printf("Wrong input");
+    }
+    
+}
